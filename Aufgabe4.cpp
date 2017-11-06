0c3a9f2470d50acaa1da02b4bb3934b0c1ed4291
Visprak30 <visprak30@jung.informatik.uni-leipzig.de>
2017-11-06 17:05:18 +0100

    Aufgabe 4 ca halb durch

--- Aufgabe4.cpp
+++ Aufgabe4.cpp
@@ -2,74 +2,93 @@
 #include <fantom/register.hpp>
 #include <fantom/fields.hpp>
 #include <fantom/graphics.hpp>
+#include <string>
 
 using namespace fantom;
 
 namespace {
-    class Integrator : public VisAlgorithm {
-
+    // my classes
+    template<class T, size_t N>//somehow asks for a template to initialize
+    class Integrator {
+        public:
+            double stepWidth;
+            //virtual Tensor<T, N> nextStep(Tensor<T, N> xn, TensorFieldContinuous<3, Tensor<double, 3>>::Evaluator& evaluator) = 0;
+            Integrator(double stepWidth){
+                this->stepWidth = stepWidth;
+                this->reset();
+            }
+            void reset(){}
+            virtual ~Integrator(){} //initialize self; virtual to pass on inheritance
     };
 
-    class Aufgabe4Algorithm : public VisAlgorithm {
-        std::unique_ptr< Primitive > mGlyphs;//universe of images
+    template<class T, size_t N>//somehow asks for a template to initialize
+    class Euler: public Integrator<T, N>{
+        public:
+            Euler(double stepWidth) : Integrator<T, N>(stepWidth){}
+            ~Euler(){}
+    };
 
-    public:
-        struct Options : public VisAlgorithm::Options {
-            Options( Control& control ) : VisAlgorithm::Options(control) {
-                // add options here
-                add<TensorFieldContinuous<3, Vector3 > >("Field", "A 3D vector field");
+    template<class T, size_t N>//somehow asks for a template to initialize
+    class RungeKutta: public Integrator<T, N>{
+        public:
+            RungeKutta(double stepWidth) : Integrator<T, N>(stepWidth){}
+             ~RungeKutta(){}
+    };
 
-            }
-        };
+    //main class
+    class Aufgabe4Algorithm: public VisAlgorithm{
+        std::unique_ptr< Primitive > mGlyphs;
 
-        Aufgabe4Algorithm( InitData & data) : VisAlgorithm(data) {
-
-        }
-
-        void execute( const Algorithm::Options& options, const volatile bool& /* abortFlag */ ) override {
-            //add your code here
-            mGlyphs = getGraphics("Glyphs").makePrimitive();
-            std::shared_ptr< const TensorFieldContinuous< 3, Vector3 > > myField = options.get< TensorFieldContinuous< 3, Vector3 > >( "Field" );
-
-            // if there is no input, do nothing
-            if( !myField ) {
-                debugLog() << "Input Field not set." << std::endl;
-                return;
-            }
-            // sanity check that interpolated fields really use the correct grid type. This should never fail
-            std::shared_ptr< const Grid< 3 > > myGrid = std::dynamic_pointer_cast< const Grid< 3 > >( myField->domain() );
-            if( !myGrid ) {
-                throw std::logic_error( "Wrong type of grid!" );
-            }
-
-            const ValueArray< Point3 >& points = myGrid->points();
-            std::vector< Vector3 > vertices(myGrid->numCells());
-            auto evaluator = myField->makeEvaluator();
-
-            for( int i = 0; myGrid ->numCells(); i++){
-                Cell cell = myGrid->cell( i );    //calculates barrycenter; not needed here but for seeing how to do it
-                Point3 point;
-
-                for( size_t j = 0; j != cell.numVertices(); ++j )
-                {
-                    point += points[cell.index( j )];
+        public:
+            struct Options : public VisAlgorithm::Options {
+                Options( Control& control ) : VisAlgorithm::Options(control) {
+                    add<TensorFieldContinuous<3, Vector3 > >("Field", "A 3D vector field");
+                    add< double >("stepWidth", "steps size made", 1.0);
+                    add< InputChoices >("method", "Chose a wizard to do the magic", std::vector<std::string>({"Euler", "RungeKutta"}), "Euler");  //default Euler
                 }
-                point /= cell.numVertices();
+            };
 
-                if( evaluator->reset( point, time ) ) { //returns True if point inside the domain
-                    auto v = evaluator->value(); // v is the value returned by the evaluator
-                    locMaximum = std::max( locMaximum, norm( v ) );
-                    vertices[i] = point;
-                    vertices[i + 1] = v;
+            struct VisOutputs : public VisAlgorithm::VisOutputs {
+                VisOutputs( fantom::VisOutputs::Control& control ): VisAlgorithm::VisOutputs( control ){
+                    addGraphics( "Glyphs" );
+                }
+            };
+
+            Aufgabe4Algorithm( InitData& data ) : VisAlgorithm( data ) {
+            }
+
+            void execute( const Algorithm::Options& options, const volatile bool& /* abortFlag */ ) override {
+                mGlyphs = getGraphics("Glyphs").makePrimitive();
+                std::shared_ptr< const TensorFieldContinuous< 3, Vector3 > > myField = options.get< TensorFieldContinuous< 3, Vector3 > >( "Field" );
+
+                // if there is no input, do nothing
+                if( !myField ) {
+                   debugLog() << "Input Field not set." << std::endl;
+                   return;
+                }
+                // sanity check that interpolated fields really use the correct grid type. This should never fail
+                std::shared_ptr< const Grid< 3 > > myGrid = std::dynamic_pointer_cast< const Grid< 3 > >( myField->domain() );
+                if( !myGrid ) {
+                    throw std::logic_error( "Wrong type of grid!" );
                 }
 
 
+                // here it begins
+                //const ValueArray< Point3 >& points = myGrid->points();
+                //std::vector< Vector3 > vertices(myGrid->numCells());
+                //auto evaluator = myField->makeEvaluator();
 
+                Integrator<double, 3> myIntegrator(options.get<double>("stepWidth"));
+
+                //choose which function
+                std::string method = options.get< std::string >("method");
+                if (method.compare("Euler")) myIntegrator = Euler<double, 3>(options.get< double >("stepWidth"));
+                else if (method.compare("RungeKutta")) myIntegrator = RungeKutta<double, 3>(options.get< double >("stepWidth"));
+
+
+
+                //mGlyphs->add( Primitive::LINES ).setColor(Color(1,1,0)).setVertices( vertices ); //draw lines
             }
-
-            mGlyphs->add( Primitive::LINES ).setColor(Color(1,0,1)).setVertices( vertices ); //draw it
-
-        }
     };
 
     AlgorithmRegister< Aufgabe4Algorithm > reg("praktikum/Aufgabe4", "Science and more science. Actually a grid with flowvectors.");
